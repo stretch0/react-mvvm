acfeab71164b3e015fee4645af10d215388a93ec
Andrew McCallum <amccallum89@gmail.com>
2023-10-21 18:21:00 +0100

    fixed build

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'module',
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      exclude: ["**/__tests__", "**/*.test.ts", "test-project"],
+      allowSyntheticDefaultImports: true
+    }),
+  ]
+};
